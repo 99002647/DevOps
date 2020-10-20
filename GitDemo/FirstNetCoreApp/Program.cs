4a26ee912b4eaf6791eadf8e2622aa146979cf9f
99002647 <pradeep.v@ltts.com>
2020-10-20 15:10:45 +0530

    Committed

--- GitDemo/FirstNetCoreApp/Program.cs
+++ GitDemo/FirstNetCoreApp/Program.cs
@@ -2,11 +2,13 @@
 
 namespace FirstNetCoreApp
 {
+    public delegate int MathOpDelegate(int a, int b);
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
         }
     }
 }
